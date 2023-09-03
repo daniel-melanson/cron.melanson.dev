78ff90ba97c2d1fe317b1db7971f04e3861cd7ae
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2023-09-03 19:29:01 -0400

    feat: footer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
+  <Footer />
 </template>
+
+<style>
+#app {
+  display: grid;
+}
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+  <footer>
+    <a href="/cron">unix man page</a>
+    <a
+      href="http://www.quartz-scheduler.org/documentation/quartz-2.3.0/tutorials/crontrigger.html"
+      target="_blank"
+      >quartz cron trigger docs</a
+    >
+    <a
+      href="http://www.quartz-scheduler.org/documentation/quartz-2.3.0/tutorials/crontrigger.html"
+      target="_blank"
+      >aws cron expressions reference</a
+    >
+    <a
+      href="https://github.com/daniel-melanson/cron.melanson.dev"
+      target="_blank"
+      >source code</a
+    >
+  </footer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+footer {
+  display: flex;
+  justify-content: center;
+}
+
+footer > a {
+  padding: 0 1em;
+}
+
+footer > a:not(:last-child) {
+  border-right: 1px solid #fffcf2;
+}
+</style>
