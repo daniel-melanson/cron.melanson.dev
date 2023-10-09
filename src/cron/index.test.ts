2f1633e8bcdd6decccb6a832283ff4e43e399fdf
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2023-10-09 17:22:48 -0400

    test: unix tests

--- src/cron/index.test.ts
+++ src/cron/index.test.ts
@@ -3,7 +3,7 @@ import { CRON_SYNTAX } from "./index";
 
 function expectText(cron: CronSyntax, expression: string, text: string) {
   const description = cron.describe(expression);
-  expect(description.success).toBe(true);
+  expect(description.ok).toBe(true);
   // @ts-ignore
   expect(description.value.text).toBe(text);
 }
@@ -11,35 +11,57 @@ function expectText(cron: CronSyntax, expression: string, text: string) {
 describe("UNIX", () => {
   const cron = CRON_SYNTAX.UNIX;
 
-  it("Correctly describe all wildcards", () => {
+  it("All wildcards", () => {
     expectText(cron, "* * * * *", "Every minute.");
   });
 
-  it("Correctly describe specific time", () => {
+  it("Specific times", () => {
     expectText(cron, "35 15 * * *", "At 15:35.");
     expectText(cron, "0 15 * * *", "At 15:00.");
     expectText(cron, "0 1 * * *", "At 1:00.");
   });
 
-  it("Correctly describe nth days", () => {
-    expectText(cron, "* * */2 * * *", "Every 2nd day.");
-    expectText(cron, "* * */10 * * *", "Every 10th day.");
-    expectText(cron, "* * */3 * * *", "Every 3rd day.");
+  it("Any time on specific days.", () => {
+    expectText(
+      cron,
+      "* * 15 10 *",
+      "Every minute of every hour on the 15th of October.",
+    );
+    expectText(
+      cron,
+      "* * 23 NOV *",
+      "Every minute of every hour on the 23rd of November.",
+    );
+  });
+
+  it("nth days", () => {
+    expectText(
+      cron,
+      "* * */2 * * *",
+      "Every minute of every hour every 2nd day.",
+    );
+    expectText(
+      cron,
+      "* * */10 * * *",
+      "Every minute of every hour every 10th day.",
+    );
+    expectText(
+      cron,
+      "* * */3 * * *",
+      "Every minute of every hour every 3rd day.",
+    );
+    expectText(
+      cron,
+      "0 0 */13th * * *",
+      "At 00:00 every 13rd day.",
+    );
   });
 });
 
 describe("AWS", () => {
   const cron = CRON_SYNTAX.AWS;
-
-  it("Correctly describe all wildcards", () => {
-    expectText(cron, "* * * * * *", "Every minute.");
-  });
 });
 
 describe("Quartz", () => {
   const cron = CRON_SYNTAX.Quartz;
-
-  it("Correctly describe all wildcards", () => {
-    expectText(cron, "* * * * * * *", "Every second.");
-  });
 });
