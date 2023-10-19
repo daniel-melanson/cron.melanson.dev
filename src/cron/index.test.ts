5ccdef35df79bd2483e15248eb4e4a1c65ca3edc
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2023-10-19 13:10:41 -0400

    refactor: tests

--- src/cron/index.test.ts
+++ src/cron/index.test.ts
@@ -2,7 +2,7 @@ import type { CronSyntax } from "./CronSyntax";
 import { CRON_SYNTAX } from "./index";
 import { CronExpressionDescription } from "./types";
 
-function expectText(cron: CronSyntax, expression: string, text: string) {
+function expectTextCron(cron: CronSyntax, expression: string, text: string) {
   const result = cron.describe(expression);
   expect(result.ok).toBe(true);
 
@@ -12,10 +12,11 @@ function expectText(cron: CronSyntax, expression: string, text: string) {
 
 describe("UNIX", () => {
   const cron = CRON_SYNTAX.UNIX;
+  const expectText = (expression: string, text: string) =>
+    expectTextCron(cron, expression, text);
 
   it("All wildcards", () => {
     expectText(
-      cron,
       "* * * * *",
       "Every minute, of every hour, of every day-of-month, of every month, on any day-of-week.",
     );
@@ -23,17 +24,14 @@ describe("UNIX", () => {
 
   it("Specific times", () => {
     expectText(
-      cron,
       "35 15 * * *",
       "At 15:35, every day-of-month, of every month, on any day-of-week.",
     );
     expectText(
-      cron,
       "0 15 * * *",
       "At 15:00, every day-of-month, of every month, on any day-of-week.",
     );
     expectText(
-      cron,
       "0 1 * * *",
       "At 1:00, every day-of-month, of every month, on any day-of-week.",
     );
@@ -41,34 +39,20 @@ describe("UNIX", () => {
 
   it("Any time on specific days.", () => {
     expectText(
-      cron,
       "* * 15 10 *",
       "Every minute, of every hour, on the 15th of October, on any day-of-week.",
     );
     expectText(
-      cron,
       "* * 23 NOV *",
       "Every minute of every hour on the 23rd of November, on any day-of-week.",
     );
   });
 
   it("nth days", () => {
-    expectText(
-      cron,
-      "* */2 * * *",
-      "Every minute of every hour every 2nd day.",
-    );
-    expectText(
-      cron,
-      "* */10 * * *",
-      "Every minute of every hour every 10th day.",
-    );
-    expectText(
-      cron,
-      "* */3 * * *",
-      "Every minute of every hour every 3rd day.",
-    );
-    expectText(cron, "0 0 */13th * * *", "At 00:00 every 13rd day.");
+    expectText("* */2 * * *", "Every minute of every hour every 2nd day.");
+    expectText("* */10 * * *", "Every minute of every hour every 10th day.");
+    expectText("* */3 * * *", "Every minute of every hour every 3rd day.");
+    expectText("0 0 */13th * * *", "At 00:00 every 13rd day.");
   });
 });
 
